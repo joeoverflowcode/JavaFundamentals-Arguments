740aa46ff11c3381b7f73252277b012be223a45e
Joe <139164599+joeoverflowcode@users.noreply.github.com>
2023-07-26 10:27:16 -0500

    Add files via upload

---
+++ UseArgument.java
@@ -0,0 +1,9 @@
+class UseArgument{
+	public static void main(String[] args){
+		
+System.out.print("Hello ");
+System.out.print(args[0]);
+System.out.println("! How are you today?");
+System.out.println("What's up " + args + ". How's the weather?");
+	}
+}
